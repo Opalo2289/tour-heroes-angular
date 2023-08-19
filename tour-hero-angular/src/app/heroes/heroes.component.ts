bd6a03ed930ecad2cf55a8248c479170e6d8e45c
Opalo1189 <enriquemaestre11@gmail.com>
2023-08-19 18:47:32 -0500

    funcion buscar por id.

--- tour-hero-angular/src/app/heroes/heroes.component.ts
+++ tour-hero-angular/src/app/heroes/heroes.component.ts
@@ -26,4 +26,20 @@ export class HeroesComponent {
     this._heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);//como se lee esto
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this._heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this._heroService.deleteHero(hero.id).subscribe();
+  }
+
+  
 }
